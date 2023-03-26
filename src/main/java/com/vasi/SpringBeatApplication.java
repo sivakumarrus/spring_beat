f20f7732fb27696eee8bbe4ded205e125e3498c6
sivak <sivak@Sivamayam>
2023-03-26 11:24:40 -0700

    changes

---
+++ src/main/java/com/vasi/SpringBeatApplication.java
@@ -0,0 +1,13 @@
+package com.vasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasi/SpringBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
